30af0b9c27915272cbd12239cca20f744b70c8bf
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-11 21:21:48 +0700

    Comparison with the Strict Inequality Operator

--- 63.Comparison with the Strict Inequality Operator.js
+++ 63.Comparison with the Strict Inequality Operator.js
@@ -1,3 +1,5 @@
+// TODO : Add the strict inequality operator to the if statement so the function will return the string Not Equal when val is not strictly equal to 17
+
 function testStrictNotEqual(val) {
   if (val !== 17) {
     return 'Not Equal';
