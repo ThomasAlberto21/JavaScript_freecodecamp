1fa4cc019cc1e052d89cbd26890f78fa99263687
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-11 21:17:11 +0700

    Comparison with the Inequality Operator

---
+++ 62.Comparison_with_the_inequality_operator.js
@@ -0,0 +1,11 @@
+// TODO : Add the inequality operator != in the if statement so that the function will return the string Not Equal when val is not equivalent to 99.
+
+function testNotEqual(val) {
+  if (val != 99) {
+    console.log('Not Equal');
+  } else {
+    console.log('Equal');
+  }
+}
+
+testNotEqual(10);
