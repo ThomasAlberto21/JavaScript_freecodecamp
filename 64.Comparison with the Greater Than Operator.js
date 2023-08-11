d8635df91af7409821e393fb393c3a8336d13706
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-11 21:26:24 +0700

    Comparison with the Greater Than Operator

---
+++ 64.Comparison with the Greater Than Operator.js
@@ -0,0 +1,15 @@
+// TODO : Add the greater than operator to the indicated lines so that the return statements make sense.
+
+function testGreaterThan(val) {
+  if (val > 100) {
+    return 'Over 100';
+  }
+
+  if (val > 10) {
+    return 'Over 10';
+  }
+
+  return '10 or Under';
+}
+
+testGreaterThan(10);
