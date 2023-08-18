d9e3aeaa4e5a7c39d7eb80a678ef78058608bf3f
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-18 18:30:03 +0700

    Delete Properties from a JavaScript Object

---
+++ 87.Delete Properties from a JavaScript Object.js
@@ -0,0 +1,12 @@
+// TODO : Delete the tails property from myDog. You may use either dot or bracket notation.
+
+// Setup
+const myDog = {
+  name: 'Happy Coder',
+  legs: 4,
+  tails: 1,
+  friends: ['freeCodeCamp Campers'],
+  bark: 'woof',
+};
+
+delete myDog.tails;
