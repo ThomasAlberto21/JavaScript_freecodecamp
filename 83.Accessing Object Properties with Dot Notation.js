8a342c85ae65cf4355c50322271b58e7ebbc1c99
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-08-18 18:13:00 +0700

    Accessing Object Properties with Dot Notation

---
+++ 83.Accessing Object Properties with Dot Notation.js
@@ -0,0 +1,17 @@
+/* 
+
+    Read in the property values of testObj using dot notation. Set the variable hatValue equal to the object's property hat and set the variable shirtValue equal to the object's property shirt.
+
+*/
+
+const myObj = {
+  hat: 'ballcap',
+  shirt: 'jersey',
+  shoes: 'cleats',
+};
+
+const hatValue = myObj.hat;
+console.log(hatValue);
+
+const shirtValue = myObj.shirt;
+console.log(shirtValue);
